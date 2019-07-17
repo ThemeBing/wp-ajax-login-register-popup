04e301d802fe86e3ce874207912ae27d10c97014
Farhan Khan <Farhan01617@gmail.com>
2019-07-17 21:30:25 +0600

    update

--- add-to-menu.php
+++ add-to-menu.php
@@ -4,7 +4,7 @@
  */
 
 function yourname_login_link_to_menu ( $items, $args ) {
-    if( ! is_user_logged_in() && $args->theme_location == apply_filters('login_menu_location', 'menu-1') ) {
+    if( ! is_user_logged_in() && $args->theme_location == apply_filters('login_menu_location', 'primary') ) {
         $items .= '<li class="menu-item"><a href="#yourname-login">'.__( 'Login/Register', 'yourname' ).'</a></li>';
     }
     return $items;
--- ajax-login-register.php
+++ ajax-login-register.php
@@ -1,12 +1,25 @@
 <?php
 
+/**
+ * Plugin Name: Ajax Login Register
+ * Plugin URI:  https://github.com/farhan01617/wp-ajax-login-register-popup
+ * Description: Handle the basics with this plugin.
+ * Version:     1.0.0
+ * Author:      Farhan Khan
+ * Author URI:  https://github.com/farhan01617
+ * License:     GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: alg
+ * Domain Path: /languages
+ */
+
 require plugin_dir_path( __FILE__ ).'add-to-menu.php';
 require plugin_dir_path( __FILE__ ).'ajax-handle.php';
 require plugin_dir_path( __FILE__ ).'form-attach-to-footer.php';
 
 function yourname_ajax_login_scripts() {
-	wp_enqueue_style( 'user-login', get_template_directory_uri().'/'.basename(__DIR__) .'/user-login.css', array(), null );
-	wp_enqueue_script( 'ajax-login-register-script', get_template_directory_uri().'/'.basename(__DIR__).'/ajax.js', array( 'jquery' ), null, true );
+	wp_enqueue_style( 'user-login', plugin_dir_url( __FILE__ ).'user-login.css', array(), null );
+	wp_enqueue_script( 'ajax-login-register-script', plugin_dir_url( __FILE__ ) .'ajax.js', array( 'jquery' ), null, true );
 	wp_localize_script('ajax-login-register-script', 'yournameajax', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) );
 }
 add_action( 'wp_enqueue_scripts', 'yourname_ajax_login_scripts' );
