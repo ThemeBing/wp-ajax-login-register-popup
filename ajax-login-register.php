8df1467f702c21ae2287712aa5c469baac766a94
Farhan Khan <farhan01617@gmail.com>
2019-07-17 14:12:19 +0600

    Update ajax-login-register.php

--- ajax-login-register.php
+++ ajax-login-register.php
@@ -1,15 +1,12 @@
-<?php 
+<?php
 
-
-require get_template_directory() . '/ajax-login-register/add-to-menu.php';
-require get_template_directory() . '/ajax-login-register/ajax-handle.php';
-require get_template_directory() . '/ajax-login-register/form-attach-to-footer.php';
+require plugin_dir_path( __FILE__ ).'add-to-menu.php';
+require plugin_dir_path( __FILE__ ).'ajax-handle.php';
+require plugin_dir_path( __FILE__ ).'form-attach-to-footer.php';
 
 function yourname_ajax_login_scripts() {
-	wp_enqueue_style( 'user-login', get_template_directory_uri() . '/ajax-login-register/user-login.css', array(), null );
-	wp_enqueue_script( 'ajax-login-register-script', get_template_directory_uri() . '/ajax-login-register/ajax.js', array( 'jquery' ), null, true );
-	wp_localize_script('ajax-login-register-script', 'yournameajax', array( 
-			    	'ajaxurl' => admin_url( 'admin-ajax.php' ),
-			    ));
+	wp_enqueue_style( 'user-login', get_template_directory_uri().'/'.basename(__DIR__) .'/user-login.css', array(), null );
+	wp_enqueue_script( 'ajax-login-register-script', get_template_directory_uri().'/'.basename(__DIR__).'/ajax.js', array( 'jquery' ), null, true );
+	wp_localize_script('ajax-login-register-script', 'yournameajax', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) );
 }
 add_action( 'wp_enqueue_scripts', 'yourname_ajax_login_scripts' );
